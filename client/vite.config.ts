f44755e216eaa284f76ae6b8e7614ed2873085e2
James Walker <james.chunho@gmail.com>
2023-09-14 00:31:28 +0100

    implement loading from the config file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import fs from "fs";
 import path from "path";
+import { parse } from "smol-toml";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  define: {
+    __API_HOST__: (() => {
+      const configPath = path.resolve(__dirname, "../config.toml");
+      const config = parse(fs.readFileSync(configPath, "utf8"));
+      const apiHost = config["client"]["host"];
+      if (typeof apiHost !== "string")
+        throw `failed to get API host from config.client.host: ${apiHost}`;
+
+      return JSON.stringify(apiHost);
+    })(),
+  },
 
   resolve: {
     alias: {
