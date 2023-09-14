a88ce633f2fe97c755beaf5f558594e9e162f9c2
James Walker <james.chunho@gmail.com>
2023-09-14 17:05:10 +0100

    remove toml dependency

--- server/lib/config.py
+++ server/lib/config.py
@@ -1,9 +1,9 @@
-import toml
+import tomli
 import os.path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 config_path = os.path.join(BASE_DIR, "config.toml")
 
-with open(config_path, "r", encoding="utf8") as f:
-    config = toml.load(f)
+with open(config_path, "rb") as f:
+    config = tomli.load(f)
